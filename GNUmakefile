7e6bb2a724b0e02e6e3351503feba24b25e2192f
Page <pjgazzard@googlemail.com>
2013-03-06 15:45:51 +0000

    Move docco gen from make to grunt.

--- GNUmakefile
+++ GNUmakefile
@@ -1,7 +1,2 @@
 test:
 	nodeunit test/unit/*-test.js
-
-docs:
-	docco lib/ometajs/*.js
-
-.PHONY: docs
--- Gruntfile.js
+++ Gruntfile.js
@@ -19,11 +19,20 @@ module.exports = function(grunt) {
 		},
 		clean: {
 			temp: ['temp']
+		},
+		docco: {
+			docs: {
+				src: ['lib/ometajs/*.js'],
+				options: {
+					output: 'docs/'
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-docco');
 	grunt.loadTasks('./tasks');
 	grunt.registerTask('default', ['concat:parsers', 'ometajs:parsers', 'clean:temp']);
 };
